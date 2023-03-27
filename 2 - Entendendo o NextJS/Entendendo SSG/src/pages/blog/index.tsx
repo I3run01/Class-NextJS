1213b44044f0e2785a48bbf9cfd5fd82db4ff4e0
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-03-27 11:32:49 -0300

    c

---
+++ 2 - Entendendo o NextJS/Entendendo SSG/src/pages/blog/index.tsx
@@ -0,0 +1,25 @@
+type Props = {
+    name: string
+}
+
+const Blog = ({name}: Props) => {
+
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>Blog de {name}</p>
+        </div>
+    )
+}
+
+export const getStaticProps = () => {
+
+    return {
+        props: {
+            name: 'Bonieky'
+        }
+    }
+    
+}
+
+export default Blog
