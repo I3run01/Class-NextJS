2616407fd0bd0a57620b62211a69b6d658f62ce5
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-03-28 09:07:43 -0300

    c

--- 2 - Entendendo o NextJS/Entendendo CSR - Client server render/src/pages/todo/index.tsx
+++ 2 - Entendendo o NextJS/Entendendo CSR - Client server render/src/pages/todo/index.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react'
 import { Todo } from '../../types/Todos'
 
-type Props = {
-    todo: Todo[]
-}
+const Todo = () => {
+    const [todoList, setTodoList] = useState<Todo[]>([])
+    const [loading, setLoading] = useState(false);
 
-const Todo = ({todo}: Props) => {
+    useEffect(() => {
+        loadTodos()
+    }, [])
+
+    const loadTodos = async () => {
+        setLoading(true)
+        const res = await fetch('https://jsonplaceholder.typicode.co./todos')
+        const list: Todo[] = await res.json()
+        setTodoList(list)
+        setLoading(false)
+    }
 
     return (
         <div>
             <h1>Lista de tarefas</h1>
 
+            {loading && <div>Carregando...</div> }
+
             <ul>
-                {todo.map((todoItem, index) => (
+                {todoList.map((todoItem, index) => (
                     <li key={index} >{todoItem.title} - {todoItem.completed.toString()}</li>
                 ))}
             </ul>
@@ -20,15 +33,3 @@ const Todo = ({todo}: Props) => {
 }
 
 export default Todo
-
-//A página carrega a cada requisição
-export const getServerSideProps = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const todoList: Todo[] = await res.json()
-
-    return {
-        props: {
-            todo: todoList
-        }
-    }
-}
